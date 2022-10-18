d6b5563a8c14990fb8835bee2570ec1f0800f596
russell <russell@touchlab.co>
2022-10-18 07:46:36 -0400

    add run_tests script

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./gradlew kmmBridgePublish --no-daemon --stacktrace -Dkotlin.incremental=false -Dorg.gradle.jvmargs="-Xmx3g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -XX:MaxMetaspaceSize=512m"
+
+cd ../ios-spm
+xcodebuild -configuration Debug -scheme KmmBridgeIntegrationTestSpm -sdk iphonesimulator
+
+cd ../ios-cocoapods
+pod install
+xcodebuild -workspace KmmBridgeIntegrationTestCocoapods.xcworkspace -configuration Debug -scheme KmmBridgeIntegrationTestCocoapods -sdk iphonesimulator
