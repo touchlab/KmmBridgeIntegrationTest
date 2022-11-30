7f4601d172bb7d511837961487552ce717ed5732
runner <runner@Mac-1669838332325.local>
2022-11-30 20:10:17 +0000

    KMM SPM package release for 1.3.5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KmmBridgeIntegrationTest/co/touchlab/shared-kmmbridge/1.3.4/shared-kmmbridge-1.3.4.zip"
-let remoteKotlinChecksum = "dccf465a9340cacab9234e1f47fbe84dff1e7376d2c217659da7db02990a4133"
+let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/KmmBridgeIntegrationTest/co/touchlab/shared-kmmbridge/1.3.5/shared-kmmbridge-1.3.5.zip"
+let remoteKotlinChecksum = "24274e92a1afa9695419bc71cc26b2b8bbd48e75334922267c357d7676bc6ebe"
 let packageName = "shared"
 
 let package = Package(
